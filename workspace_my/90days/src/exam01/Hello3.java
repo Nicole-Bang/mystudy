ab8bcd5096821e785f1dee0944dba872de59f36d
Nicole_B <bubble4250@gmail.com>
2023-02-22 13:13:26 +0900

    first commit

---
+++ workspace_my/90days/src/exam01/Hello3.java
@@ -0,0 +1,25 @@
+package exam01;
+
+public class Hello3 {
+
+	//필드 정의
+	int age;
+	//int -> 키워드
+	//age -> 식별자(변수)
+	
+	int myAge = 10;		//바로 정의 가능
+	
+	//메서드(함수)
+	
+	//두개의 숫자값을 입력받아서 높은 값을 구하는 메서드
+	//매개변수	(필수는 아니다.)
+	//리턴타입	(필수는 아니다.)
+	//f(x)= x + 1; 
+	int maxNumber(int x, int y) {
+		return 0;
+	}
+	
+	void maxNumber() {
+	
+	}	//리턴 없이도 void를 이용하면 사용 가능
+}
